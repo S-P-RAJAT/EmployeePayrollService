107e187c263ef8afa2a2ab648805fcb3a9672361
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-19 20:59:24 +0530

    ADDED: Watch service to watch directory & its files for any changes

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/WatchServiceExample.java
@@ -0,0 +1,81 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+import static java.nio.file.StandardWatchEventKinds.*;
+
+
+
+public class WatchServiceExample {
+    private final WatchService watcher;
+    private final Map<WatchKey, Path> dirWatchers;
+
+    WatchServiceExample(Path dir) throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.dirWatchers = new HashMap<WatchKey, Path>();
+        scanAndRegisterDirectories(dir);
+    }
+
+    private void registerDirWatchers(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        dirWatchers.put(key, dir);
+    }
+
+    private void scanAndRegisterDirectories(final Path start) throws IOException {
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir,BasicFileAttributes attrs) throws IOException {
+                registerDirWatchers(dir) ;
+                return FileVisitResult.CONTINUE;
+            }});
+
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void processEvents() {
+        while (true) {
+            WatchKey key;
+            try {
+                key = watcher.take();
+            }
+            catch (InterruptedException x) {
+                return;
+            }
+            Path dir = dirWatchers.get(key);
+            if (dir == null) continue;
+            for (WatchEvent<?> event : key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+                Path name = ((WatchEvent<Path>) event) .context();
+                Path child = dir.resolve(name) ;
+                System.out.format("%s: %s\n", event.kind().name(), child); // print out event
+
+                if (kind == ENTRY_CREATE) {
+                    try {
+                        if (Files.isDirectory(child)) scanAndRegisterDirectories(child);
+                    }
+                    catch (IOException X) { }
+                }
+                else if (kind.equals(ENTRY_DELETE)) {
+                    if (Files.isDirectory(child)) dirWatchers. remove(key) ;
+                }
+            }
+
+            boolean valid = key.reset();
+            if (!valid) {
+                dirWatchers. remove(key);
+                if (dirWatchers.isEmpty())break;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayrollservice/WatchServiceTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class WatchServiceTest {
+    private static String HOME = System.getProperty("user.home");
+    private static String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenADirectoryWhenWatchedListsAllActivities() throws IOException{
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new WatchServiceExample(dir).processEvents();
+    }
+}
