c7ff22f499b40d4e8bf41194b934734f38030f3d
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-26 01:55:50 +0530

    ADDED: Method to list all registered drivers

--- src/main/java/com/bridgelabz/employeepayrollservice/DBDemo.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/DBDemo.java
@@ -1,21 +1,32 @@
 package com.bridgelabz.employeepayrollservice;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
 public class DBDemo {
 
     public static void main(String[] args) {
 
-        String jdbcURL="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
-        String userName="rajat";
-        String password="Mysql@3q#";
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "rajat";
+        String password = "Mysql@3q#";
         Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
-        }catch(ClassNotFoundException e) {
-            throw new IllegalStateException("lol",e);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("lol", e);
         }
+        listDrivers();
     }
 
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println(" " + driverClass.getClass().getName());
+        }
+    }
 }
