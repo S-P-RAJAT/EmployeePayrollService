72f5f84894a10f1aa6ba83932c2ce89d30dcee92
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-28 21:47:22 +0530

    ADDED: Testcase to check if DB in sync with list after record insertion

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.employeepayrollservice;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -20,22 +23,23 @@ public class EmployeePayrollServiceTest {
     public static void beforeClass() {
         employeePayrollService = new EmployeePayrollService();
         try {
-            employeePayrollData= employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-        } catch (EmployeePayrollException e){
-            assertEquals(EmployeePayrollException.ExceptionType.UNKNOWN_DATABASE,e.type);
+            employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+        } catch (EmployeePayrollException e) {
+            assertEquals(EmployeePayrollException.ExceptionType.UNKNOWN_DATABASE, e.type);
             e.printStackTrace();
         }
     }
 
     @Test
-    public void givenDatabase_WhenWrong_ShouldThrowCustomException(){
+    public void givenDatabase_WhenWrong_ShouldThrowCustomException() {
         try {
-            employeePayrollData= employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-        } catch (EmployeePayrollException e){
-            assertEquals(EmployeePayrollException.ExceptionType.UNKNOWN_DATABASE,e.type);
+            employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+        } catch (EmployeePayrollException e) {
+            assertEquals(EmployeePayrollException.ExceptionType.UNKNOWN_DATABASE, e.type);
             e.printStackTrace();
         }
     }
+
     @Test
     public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
         EmployeePayrollData[] arrayOfEmps = {
@@ -51,28 +55,30 @@ public class EmployeePayrollServiceTest {
     }
 
     @Test
-    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() throws EmployeePayrollException{
-        List<EmployeePayrollData> employeePayrollData= employeePayrollService.readEmployeePayrollData(IOService.FILE_IO);
+    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() throws EmployeePayrollException {
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.FILE_IO);
         assertEquals(3, employeePayrollData.size());
     }
+
     @Test
     public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
         assertEquals(4, employeePayrollData.size());
 
     }
+
     @Test
     public void givenNewSalaryForEmployee_WhenUpdates_ShouldSyncWithDB() {
 
         try {
-            employeePayrollData= employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+            employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
             employeePayrollService.printData(IOService.FILE_IO);
             employeePayrollService.updateEmployeeSalary("Terisa", 400000.00);
             boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
             assertTrue(result);
-        } catch (EmployeePayrollException e){
-            assertEquals(EmployeePayrollException.ExceptionType.UNSUCCESSFUL_UPDATE,e.type);
+        } catch (EmployeePayrollException e) {
+            assertEquals(EmployeePayrollException.ExceptionType.UNSUCCESSFUL_UPDATE, e.type);
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -81,41 +87,55 @@ public class EmployeePayrollServiceTest {
     public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
         String startDate = "2018-01-01";
         String endDate = "2019-12-01";
-        List<EmployeePayrollData> employeesListInDateRange = employeePayrollService.getEmployeesFromDateRange(startDate,endDate);
+        List<EmployeePayrollData> employeesListInDateRange = employeePayrollService.getEmployeesFromDateRange(startDate, endDate);
         assertEquals(2, employeesListInDateRange.size());
     }
 
     @Test
     public void givenGender_WhenRetrieved_ShouldGetSumOFSalaryBasedOnGender() {
         Map<String, Double> sum = employeePayrollService.getSalarySumBasedOnGender();
-        assertEquals(4000000, sum.get("M"),0.0);
-        assertEquals(400000, sum.get("F"),0.0);
+        assertEquals(4000000, sum.get("M"), 0.0);
+        assertEquals(400000, sum.get("F"), 0.0);
     }
 
     @Test
     public void givenGender_WhenRetrieved_ShouldGetAverageSalaryBasedOnGender() {
         Map<String, Double> average = employeePayrollService.getAverageSalaryBasedOnGender();
-        assertEquals(2000000, average.get("M"),0.0);
-        assertEquals(400000, average.get("F"),0.0);
+        assertEquals(2000000, average.get("M"), 0.0);
+        assertEquals(400000, average.get("F"), 0.0);
     }
 
     @Test
     public void givenGender_WhenRetrieved_ShouldGetEmployeeCountBasedOnGender() {
         Map<String, Double> count = employeePayrollService.getEmployeeCountBasedOnGender();
-        assertEquals(2, count.get("M"),0.0);
-        assertEquals(1, count.get("F"),0.0);
+        assertEquals(2, count.get("M"), 0.0);
+        assertEquals(1, count.get("F"), 0.0);
     }
 
     @Test
     public void givenGender_WhenRetrieved_ShouldGetMinimumSalaryBasedOnGender() {
         Map<String, Double> minimumSalary = employeePayrollService.getMinimumSalaryBasedOnGender();
-        assertEquals(1000000, minimumSalary.get("M"),0.0);
-        assertEquals(400000, minimumSalary.get("F"),0.0);
+        assertEquals(1000000, minimumSalary.get("M"), 0.0);
+        assertEquals(400000, minimumSalary.get("F"), 0.0);
     }
+
     @Test
     public void givenGender_WhenRetrieved_ShouldGetMaximumSalaryBasedOnGender() {
         Map<String, Double> maximumSalary = employeePayrollService.getMaximumSalaryBasedOnGender();
-        assertEquals(3000000, maximumSalary.get("M"),0.0);
-        assertEquals(400000, maximumSalary.get("F"),0.0);
+        assertEquals(3000000, maximumSalary.get("M"), 0.0);
+        assertEquals(400000, maximumSalary.get("F"), 0.0);
+    }
+
+    @Test
+    public void givenNewEmployee_WhenAdded_ShouldSyncWithDB() {
+        try {
+            employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+            LocalDate startDate = LocalDate.parse("2008-09-01");
+            employeePayrollService.addEmployee("Bruce", "M", 100000.0, startDate);
+            boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+            Assert.assertTrue(result);
+        } catch (EmployeePayrollException e) {
+            e.printStackTrace();
+        }
     }
 }
